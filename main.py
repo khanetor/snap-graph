80ab2fe8e0d5827e7a2a7ff66c479d807652760a
Kha Nguyen <nlhkha@gmail.com>
2016-12-03 13:50:35 +0200

    mean distance

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
+from __future__ import division
 import snap
 
 G1 = snap.LoadEdgeList(snap.PNGraph, 'Wiki-Vote.txt', 0, 1)
+directed = True
 
-for n in G1.Nodes():
-    print n
+G1_LSCC = snap.GetMxScc(G1)
+print 'Nodes: %d' % G1_LSCC.GetNodes()
+print 'Edges: %d' % G1_LSCC.GetEdges()
 
-for e in G1.Edges():
-    print e
+# Find mean, median, diameter, effective diameter
+distance_counter = snap.TIntH()
+
+for n in G1_LSCC.Nodes():
+    n_id = n.GetId()
+    shortest_distances = snap.TIntH()
+    snap.GetShortPath(G1, n_id, shortest_distances, directed)
+
+    for i in shortest_distances:
+        if shortest_distances[i] > 0 and distance_counter.IsKey(shortest_distances[i]):
+            distance_counter[shortest_distances[i]] += 1
+        else:
+            distance_counter[shortest_distances[i]] = 1
+
+# Mean
+total_distance = 0
+total_path = 0
+for i in distance_counter:
+    total_distance += i * distance_counter[i]
+    total_path += distance_counter[i]
+
+print 'Mean: %f' % (total_distance / total_path)
