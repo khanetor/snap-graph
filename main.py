af1363842e4a6c9185a4c50310998102ad631842
Kha Nguyen <nlhkha@gmail.com>
2016-12-04 01:41:10 +0200

    modularize functions
    Signed-off-by: Kha Nguyen <nlhkha@gmail.com>

--- main.py
+++ main.py
@@ -1,62 +1,14 @@
 from __future__ import division
 import snap
+import question1
 
 G1 = snap.LoadEdgeList(snap.PNGraph, 'Wiki-Vote.txt', 0, 1)
-directed = True
 
-G1_LSCC = snap.GetMxScc(G1)
-print 'Nodes: %d' % G1_LSCC.GetNodes()
-print 'Edges: %d' % G1_LSCC.GetEdges()
+nodes, edges, mean, median, diameter, eff_diameter = question1.statistics_directed(G1)
 
-# Find mean, median, diameter, effective diameter
-distance_counter = snap.TIntH()
-
-for n in G1_LSCC.Nodes():
-    n_id = n.GetId()
-    shortest_distances = snap.TIntH()
-    snap.GetShortPath(G1_LSCC, n_id, shortest_distances, directed)
-
-    for i in shortest_distances:
-        if shortest_distances[i] <= 0:
-            continue
-
-        if distance_counter.IsKey(shortest_distances[i]):
-            distance_counter[shortest_distances[i]] += 1
-        else:
-            distance_counter[shortest_distances[i]] = 1
-
-total_distance = 0
-total_distance_count = 0
-for i in distance_counter:
-    total_distance += i * distance_counter[i]
-    total_distance_count += distance_counter[i]
-
-# Mean
-mean = total_distance / total_distance_count
+print 'Nodes: %d' % nodes
+print 'Edges: %d' % edges
 print 'Mean: %f' % mean
-
-# Median
-i = -1
-half = 0
-half_total_distance_count = total_distance_count / 2
-while half <= half_total_distance_count:
-    i += 1
-    half += distance_counter[distance_counter.GetKey(i)]
-
-median = distance_counter.GetKey(i)
 print 'Median: %d' % median
-
-# Diameter
-diameter = distance_counter.GetKey(distance_counter.Len() - 1)
 print 'Diameter: %d' % diameter
-
-# Eff Diameter
-i = -1
-eff_quantile = 0
-eff_quantile_distance_count = total_distance_count * 0.9
-while eff_quantile <= eff_quantile_distance_count:
-    i += 1
-    eff_quantile += distance_counter[distance_counter.GetKey(i)]
-
-eff_diameter = distance_counter.GetKey(i)
 print 'Effective diameter: %d' % eff_diameter
---
+++ question1.py
@@ -0,0 +1,68 @@
+from __future__ import division
+import snap
+
+
+def statistics_directed(graph):
+    if not isinstance(graph, snap.PNGraph):
+        raise NotAGraphError(graph)
+
+    is_directed = True
+    lscc = snap.GetMxScc(graph)
+
+    nodes = lscc.GetNodes()
+    edges = lscc.GetEdges()
+
+    # Find mean, median, diameter, effective diameter
+    distance_counter = snap.TIntH()
+
+    for n in lscc.Nodes():
+        n_id = n.GetId()
+        shortest_distances = snap.TIntH()
+        snap.GetShortPath(lscc, n_id, shortest_distances, is_directed)
+
+        for i in shortest_distances:
+            if shortest_distances[i] <= 0:
+                continue
+
+            if distance_counter.IsKey(shortest_distances[i]):
+                distance_counter[shortest_distances[i]] += 1
+            else:
+                distance_counter[shortest_distances[i]] = 1
+
+    total_distance = 0
+    total_distance_count = 0
+    for i in distance_counter:
+        total_distance += i * distance_counter[i]
+        total_distance_count += distance_counter[i]
+
+    # Mean
+    mean = total_distance / total_distance_count
+
+    # Median
+    i = -1
+    half = 0
+    half_total_distance_count = total_distance_count / 2
+    while half <= half_total_distance_count:
+        i += 1
+        half += distance_counter[distance_counter.GetKey(i)]
+
+    median = distance_counter.GetKey(i)
+
+    # Diameter
+    diameter = distance_counter.GetKey(distance_counter.Len() - 1)
+
+    # Eff Diameter
+    i = -1
+    eff_quantile = 0
+    eff_quantile_distance_count = total_distance_count * 0.9
+    while eff_quantile <= eff_quantile_distance_count:
+        i += 1
+        eff_quantile += distance_counter[distance_counter.GetKey(i)]
+
+    eff_diameter = distance_counter.GetKey(i)
+
+    return nodes, edges, mean, median, diameter, eff_diameter
+
+
+class NotAGraphError(Exception):
+    pass
