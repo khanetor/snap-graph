62782ea9fdb87f08c403275a33b343f85f505f06
Hieu Nguyen <nth.itman@gmail.com>
2016-12-04 12:45:06 +0200

    Add the skeleton for stats.py

---
+++ stats.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  4 12:38:48 2016
+
+@author: tofunth
+"""
+
+# compute the median distance of the graph
+def get_median_distance(G):
+    pass
+
+
+# compute the mean distance of the graph
+def get_mean_distance(G):
+    pass
+
+
+# compute the diameter of the graph
+def get_diameter(G):
+    pass
+
+
+# compute the effective diameter of the graph
+def get_effective_diameter(G):
+    pass
+    
