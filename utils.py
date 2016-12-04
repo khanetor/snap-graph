f18b2da2c29b7eea2a0f37f72d39c5cf307fb1f5
Hieu Nguyen <nth.itman@gmail.com>
2016-12-04 12:09:53 +0200

    Put some utils skeleton codes

---
+++ utils.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  4 11:56:35 2016
+
+@author: tofunth
+"""
+
+# load the graph from text files
+# directed: indicates the graph is directed or not
+# prefix: the path of the data
+def load_graph(filename, prefix, directed=True):
+    pass
+
+
+# convert the graph from directed into undirected
+def convert_undirected(G1):
+    pass
+
+
+# save the result to file
+def save_statistics(filename, med_dist, mean_dist, diam, eff_diam):
+    pass
