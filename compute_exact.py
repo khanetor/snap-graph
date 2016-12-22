0a49199ab0e95d985168dd93be79adf7afff247e
Hieu Nguyen <nth.itman@gmail.com>
2016-12-22 03:56:08 +0200

    Add computing the exact stats (only test with wiki-Vote and quite slow). Did not put the efficient diameter yet

--- compute_exact.py
+++ compute_exact.py
@@ -5,16 +5,30 @@ Created on Sun Dec  4 12:23:40 2016
 @author: tofunth
 """
 
-from utils import load_graph, convert_undirected
-import snap
+from utils import load_graph, convert_undirected, save_statistics
 from stats import get_stats
+import os
 # %% prepare the data
+data = [
+    'wiki-Vote.txt', 'soc-Epinions1.txt','gplus_combined.txt', 
+    'soc-pokec-relationships.txt', 'soc-LiveJournal1.txt'
+    ]
+
 prefix = '../data'
-filename = 'wiki-Vote.txt'
+res_prefix = '../results'
 # %% load the data as directed graphs and compute the statistics
-G = load_graph(filename, prefix, directed=True)
-print 'Number of edge =', snap.CntUniqDirEdges(G)
-(mean, median, diameter) = get_stats(G, directed=True)
-#%% converts the graphs to undirected and compute the statistics
-G_u = convert_undirected(G)
-(mean_u, median_u, diameter_u) = get_stats(G_u, directed=False)
+for filename in data:
+    print 'processing '+filename+'...'
+    # compute directed version
+    G = load_graph(filename, prefix, directed=True)
+    (mean, median, diameter) = get_stats(G, directed=True)
+    # save the data
+    filen, fileext = os.path.splitext(filename)
+    res_filename = filen+'-directed'+fileext
+    save_statistics(res_filename, res_prefix, mean, median, diameter, 0)
+    print 'computing exact stats for '+filename+' (directed) completes!'
+    # compute undirected version
+    G_u = convert_undirected(G)
+    (mean_u, median_u, diameter_u) = get_stats(G_u, directed=False)
+    res_filename = filen+'-undirected'+fileext
+    save_statistics(res_filename, res_prefix, mean_u, median_u, diameter_u, 0)
