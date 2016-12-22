22ae7652d245ec85a1d12dfe1a9b46a8c730dfc7
Hieu Nguyen <nth.itman@gmail.com>
2016-12-22 03:54:27 +0200

    Add creating the directory if it does not exist yet

--- utils.py
+++ utils.py
@@ -5,17 +5,17 @@ Created on Sun Dec  4 11:56:35 2016
 @author: tofunth
 """
 import snap
-from os import path
+import os
 
 # load the graph from text files
 # directed: indicates the graph is directed or not
 # prefix: the path of the data
 def load_graph(filename, prefix, directed=True):
     # get the path to the file
-    filepath = path.join(prefix, filename)
-    filen, fileext = path.splitext(filepath)
+    filepath = os.path.join(prefix, filename)
+    filen, fileext = os.path.splitext(filepath)
     # check whether the file exists and is a text
-    assert path.isfile(filepath)==True and fileext=='.txt'
+    assert os.path.isfile(filepath)==True and fileext=='.txt'
     # load the graph from file, the last two params representing src and dest columns
     G = snap.LoadEdgeList(snap.PNGraph, filepath, 0, 1)
     return G
@@ -30,8 +30,10 @@ def convert_undirected(G1):
 # save the result to file
 def save_statistics(filename, prefix, med_dist, mean_dist, diam, eff_diam):
     # get the path to the file
-    filepath = path.join(prefix, filename)
-    filen, fileext = path.splitext(filepath)
+    filepath = os.path.join(prefix, filename)
+    filen, fileext = os.path.splitext(filepath)
     assert fileext=='.txt'
+    if not os.path.exists(prefix):
+        os.makedirs(prefix)
     with open(filepath, 'w') as f:
         f.write('{} {} {} {}'.format(med_dist, mean_dist, diam, eff_diam))
